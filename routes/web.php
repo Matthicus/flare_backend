21b68fd9b6d1f4cb3cec23f19ceb17c0293a0e09
Matthice Storms <matthice.storms@hotmail.com>
2025-06-11 14:59:31 +0200

    deployment attempt

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,15 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return '✅ Migrations complete!';
 });
 
 
