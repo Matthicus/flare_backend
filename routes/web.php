99d8ba44d61ff4d7b3473b825e831d221dd28b0a
Matthice Storms <matthice.storms@hotmail.com>
2025-06-11 15:23:47 +0200

    e

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,21 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-Route::get('/run-migrations', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return '✅ Migrations complete!';
+// Route::get('/run-migrations', function () {
+//     Artisan::call('migrate', ['--force' => true]);
+//     return '✅ Migrations complete!';
+// });
+
+Route::get('/db-tables', function () {
+    $tables = \DB::select('SELECT tablename FROM pg_tables WHERE schemaname = current_schema()');
+    return response()->json($tables);
 });
 
 
